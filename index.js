8f5620a929026948c155a2f982f9ef5e300338bf
Alex Sánchez <alxsnchez@gmail.com>
2018-10-12 20:14:38 +0200

    Basic server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Monitoring tool made using only Node.js APIs");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello World\n");
+});
+
+server.listen(3000, () => {
+  console.log("The server is listening on port 3000");
+});

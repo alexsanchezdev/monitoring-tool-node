cff980528948bc4e629861b502aa25fa6ebea880
Alex Sánchez <alxsnchez@gmail.com>
2018-10-12 21:15:06 +0200

    Headers parsing

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
-
+  const queryStringObject = parsedUrl.query;
   const method = req.method.toUpperCase();
+  const headers = req.headers;
 
   res.end("Hello World\n");
-  console.log(`Path: '${trimmedPath}' Method: ${method}`);
+  console.log("Headers: " + JSON.stringify(headers));
 });
 
 server.listen(3000, () => {
